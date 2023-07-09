b333ee78603821f99155287098b0c33090ce7ccb
Rau-36 <raul363636r36d@gmail.com>
2023-07-09 19:34:59 +0400

    Add Calc

---
+++ Calculator/Calculator/Calc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+
+    public class Calc
+    {
+        public int? FirstNumber { get; set; }
+        public int? SecondNumber { get; set; }
+        private char? action;
+
+        public char? Action
+        {
+            get { return action; }
+            set
+            {
+                if (value == '-' || value == '+' || value == '/' || value == '*')
+                    action = value;
+                else
+                    throw new Exception("Impossible Action");
+            }
+        }
+
+        
+        public double GetResult()
+        {
+            if (action.HasValue)
+                throw new Exception("no action assigned");
+
+            if (this.FirstNumber.HasValue == false || this.SecondNumber.HasValue == false)
+                throw new Exception("not all fields are initialized");
+
+            switch (this.Action)
+            {
+                case '+':
+                    return this.FirstNumber.Value + this.SecondNumber.Value;
+                case '-':
+                    return this.FirstNumber.Value - this.SecondNumber.Value;
+                case '*':
+                    return this.FirstNumber.Value * this.SecondNumber.Value;
+                case '/':
+                    return this.FirstNumber.Value / this.SecondNumber.Value;
+                default:
+                    throw new Exception("no action assigned");
+            }
+        }
+    }
+}
+        
+    
--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -1,7 +1,12 @@
+using System.Text;
+
 namespace Calculator
 {
     public partial class Form1 : Form
     {
+        StringBuilder? nunmber1;
+        StringBuilder? nunmber2;
+        Calc calc;
         public Form1()
         {
             InitializeComponent();
